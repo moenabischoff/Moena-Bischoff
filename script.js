27ee61e9bd8152c2fa7033554293b42a0e31ccf8
moenabischoff <moena.bischoff@gmail.com>
2025-05-26 16:26:52 +0200

    neu

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 const container = document.querySelector("#fakt");
 const button = document.querySelector("#button");
-
 const API_URL = 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=de';
 
-async function fetchData(url) {
+async function showData() {
     try {
-        const response = await fetch(url);
-        return await response.json();
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        container.innerHTML = `<p>${data.text}</p>`;
     } catch (e) {
         console.error(e);
-        return [];
     }
 }
 
-
-async function showData() {
-    let myData = await fetchData(API_URL);
-    container.innerHTML = `
-        <p>${myData.text}</p>
-        `;
-}
-
 showData();
-
-button.addEventListener("click", function (event) {
-    showData();
-});
+button.addEventListener("click", showData);
